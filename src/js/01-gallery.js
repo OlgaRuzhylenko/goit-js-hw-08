cbfaed32f6b7e5e65f6c3e9b45ffac9fdf7a9f23
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-22 23:06:00 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item"> 
+    <a href="${original}" class="gallery__link">
+    <img src="${preview}" alt="${description}"/>
+    </a>
+    </li>`).join('')
+container.insertAdjacentHTML('beforeend', markup);
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
