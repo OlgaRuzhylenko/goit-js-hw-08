e9bdfee73b8e0eeb06d0ad9fdc2ed0cd95aa378d
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-23 22:07:59 +0300

    3 завдання,1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const message = document.querySelector('textarea');
+const obj = {
+    email: '',
+    message: ''
+};
+
+form.addEventListener('input', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    if (evt.target === emailInput) {
+        onEmailInput(evt);
+    } else if (evt.target === message) {
+        onTextareaInput(evt);
+    }
+
+function onEmailInput(evt) {
+    const emailValue = evt.target.value;
+    obj.email = emailValue;    
+};
+
+function onTextareaInput(evt) {
+const messageValue = evt.target.value;
+  obj.message = messageValue
+    };
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj))
+  };
 
