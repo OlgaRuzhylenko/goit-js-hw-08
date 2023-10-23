c7b1d70b64e00acb21d1257fc597c73f105cd49a
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-23 20:35:49 +0300

    друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    const currentSeconds = data.seconds;
+    console.log(currentSeconds);
+    const currentTime = 'current_time';
+    localStorage.setItem(currentTime, JSON.stringify(currentSeconds));
+};
+
+function getCurrentTimeFromLocalStorage() {
+    const currentTime = localStorage.getItem('current_time')
+
+    if (currentTime) {
+        const timeOfPlayback = JSON.parse(currentTime);
+
+        player.setCurrentTime(timeOfPlayback).then(function (seconds) {
+            console.log("поточний час");
+        }).catch(function (error) {
+            switch (error.name) {
+                case 'RangeError':
+                    console.log("час менше 0 або більше тривалості відео")
+                    break;
+
+                default:
+                    console.log("інша помилка")
+                    break;
+            }
+        });
+
+    }
+};
+
+
+player.on('play', onPlay);
+
 
